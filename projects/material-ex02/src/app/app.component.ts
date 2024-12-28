236443ead065fd4e6ff3badc9014370ddafde8be
k.osawa <saataysu4408999@gmail.com>
2024-12-28 20:18:19 +0900

    modified

--- projects/material-ex02/src/app/app.component.ts
+++ projects/material-ex02/src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, effect, model, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NaviComponent } from "./navi/navi.component";
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 //import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
@@ -9,10 +11,23 @@ import { CommonModule } from '@angular/common';
   imports: [ 
     RouterOutlet,
      NaviComponent,
+     MatIconModule,
+     MatButtonModule,
       CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'material-ex02';
+
+  collapsed = false;
+  
+  darkMode = signal(false);
+
+  constructor(){    
+  }
+  setDarkMode = effect(() => {
+    document.documentElement.classList.toggle('dark',this.darkMode());
+  })
+  
 }
