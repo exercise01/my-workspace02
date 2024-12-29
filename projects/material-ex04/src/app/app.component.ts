098b887e1587aaa775f45eeef9f2e2fc07bb6c19
k.osawa <saataysu4408999@gmail.com>
2024-12-30 08:59:08 +0900

    modihied material-ex04

--- projects/material-ex04/src/app/app.component.ts
+++ projects/material-ex04/src/app/app.component.ts
@@ -2,14 +2,22 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NaviComponent } from "./navi/navi.component";
 import { CommonModule } from '@angular/common';
+import { TableComponent } from "./table/table.component";
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TreeComponent } from "./tree/tree.component";
+import { DragDropComponent } from "./drag-drop/drag-drop.component";
 
 @Component({
   selector: 'app-root',
   imports: [
-       RouterOutlet,
-       NaviComponent,
-       CommonModule,
-      ],
+    RouterOutlet,
+    NaviComponent,
+    CommonModule,
+    TableComponent,
+    DashboardComponent,
+    TreeComponent,
+    DragDropComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
